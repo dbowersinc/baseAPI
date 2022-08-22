98967fd9239737d342a967da6e8eca96ba315893
dbowersinc <dbowersinc@hotmail.com>
2022-08-21 20:37:53 -0700

    updated for Postman

--- baseAPI/cafes_api.py
+++ baseAPI/cafes_api.py
@@ -3,6 +3,7 @@ from .cafes_table import Cafe
 import random
 from .db import db
 from .cafes_table import Cafe
+from .cafes_forms import NewCafeForm, EditCafeForm
 
 bp = Blueprint('cafes', __name__)
 
@@ -59,8 +60,38 @@ def search():
             cafe_list.append(cafe.to_dict())
 
     return jsonify(cafe_list)
+
+
 ## HTTP POST - Create Record
+@bp.route('/add', methods=['POST'])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
 
 ## HTTP PUT/PATCH - Update Record
+@bp.route('/update-price/<cafe_id>', methods=['PATCH'])
+def update_price(cafe_id):
+    new_price = request.args.get('new_price')
+    cafe = db.session.query(Cafe).get(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."})
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+
 
 ## HTTP DELETE - Delete Record
