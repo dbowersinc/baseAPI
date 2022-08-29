6732bf06fabf66ec7ef7e73ce053dd3e3fcba377
David Bowers <dbowersinc@hotmail.com>
2022-08-28 20:30:47 -0700

    install and configure webpack

--- baseAPI/__init__.py
+++ baseAPI/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 def create_app(test_config=None):
 
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask('baseAPI', instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         SQLALCHEMY_DATABASE_URI='sqlite:///../cafes.db',
