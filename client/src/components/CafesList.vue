21e21afe692591b1daf41358e01dda65a1ad65aa
David Bowers <dbowersinc@hotmail.com>
2022-09-24 20:36:51 -0700

    show fetched data from backend

--- client/src/components/CafesList.vue
+++ client/src/components/CafesList.vue
@@ -1,18 +1,26 @@
 <script setup>
 import { reactive, ref } from "vue";
 
-const msg = ref({ text: 'This is a test of the msg text system. Is this thing on?'})
+const cafe = ref({ text: 'This is a test of the msg text system. Is this thing on?'})
+const count = ref(0)
 
+function increment() {
+  count.value++
+}
 function changeMsg() {
-  msg.text = 'And now this.'
+  cafe.value.text = 'Testing.'
+  count.value++
 }
 
 </script>
 
 <template>
 Cafes this is a test.
-  <p>{{ msg.text }}</p>
+  <p>{{ cafe.text }}</p>
   <button @click="changeMsg">Change Text</button>
+
+  <p>{{ count }}</p>
+  <button @click="increment">Inc</button>
 </template>
 
 
--- client/src/views/CafesView.vue
+++ client/src/views/CafesView.vue
@@ -1,10 +1,20 @@
 <script setup>
 import CafesList from "../components/CafesList.vue";
+import { useFetch } from "@/components/fetch"
 
+
+const { data, error } = useFetch( 'http://127.0.0.1:5000/random')
 </script>
 
 <template>
 <main>
+  <div v-if="error">Damn It!!: {{ error.message }}</div>
+  <div v-else-if="data">
+    <h1>Data</h1>
+    <pre>{{ data }}</pre>
+  </div>
+  <div v-else>Loading...</div>
+
  <CafesList />
 </main>
 </template>
