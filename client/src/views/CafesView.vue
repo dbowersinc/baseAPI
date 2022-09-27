f9084337bbcdae37dff76368c4e2f5a87ef9ce6f
David Bowers <dbowersinc@hotmail.com>
2022-09-26 18:07:07 -0700

    modal created

--- client/src/views/CafesView.vue
+++ client/src/views/CafesView.vue
@@ -1,13 +1,37 @@
 <script setup>
+import {ref} from "vue";
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
 import CafesList from "../components/CafesList.vue";
 import { useFetch } from "@/components/fetch"
 
 
 const { data, error } = useFetch( 'http://127.0.0.1:5000/random')
+const isOpen = ref(true)
+
+function closeModal() {
+  isOpen.value = false
+}
+function openModal() {
+  isOpen.value = true
+}
+
 </script>
 
 <template>
 <main>
+    <button
+      type="button"
+      @click="openModal"
+      class="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+    >
+      Open dialog
+    </button>
   <div v-if="error">Damn It!!: {{ error.message }}</div>
   <div v-else-if="data">
     <h1>Data</h1>
@@ -15,10 +39,74 @@ const { data, error } = useFetch( 'http://127.0.0.1:5000/random')
   </div>
   <div v-else>Loading...</div>
 
+  <p>{{ title }}</p>
+
  <CafesList />
 </main>
+    <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-200 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-gray-900"
+              >
+                Payment successful
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+
+                </p>
+              </div>
+              <div class="mt-4">
+                <form @submit="onSubmit" @reset="onReset">
+                  <label for="title">Title</label>
+                  <input v-model="title"/>
+                </form>
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  Got it, thanks!
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
 <style scoped>
 
 </style>
+
+
